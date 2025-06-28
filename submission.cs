37524deda5ed293c336ed8dfe9f64265237d571a
dankim0101 <dkim256@asu.edu>
2025-06-27 23:58:25 -0700

    Update submission.cs

--- submission.cs
+++ submission.cs
@@ -8,30 +8,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        string xmlPath = "Hotels.xml";
-        string xsdPath = "Hotels.xsd";
-        string xslPath = "Hotels.xsl";
-        string outputPath = "Hotels.html";
-
-        // 1. Validate XML against Schema
-        XmlReaderSettings settings = new XmlReaderSettings();
-        settings.Schemas.Add(null, xsdPath);
-        settings.ValidationType = ValidationType.Schema;
-        settings.ValidationEventHandler += new ValidationEventHandler(ValidationCallback);
-
-        using (XmlReader reader = XmlReader.Create(xmlPath, settings))
+        try
         {
-            while (reader.Read()) { }
+            string xmlPath = "Hotels.xml";
+            string xsdPath = "Hotels.xsd";
+            string xslPath = "Hotels.xsl";
+            string outputPath = "Hotels.html";
+
+            // 1. Validate XML against Schema
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.Schemas.Add(null, xsdPath);
+            settings.ValidationType = ValidationType.Schema;
+            settings.ValidationEventHandler += new ValidationEventHandler(ValidationCallback);
+
+            using (XmlReader reader = XmlReader.Create(xmlPath, settings))
+            {
+                while (reader.Read()) { }
+            }
+
+            Console.WriteLine("XML validation complete.");
+
+            // 2. Transform XML to HTML using XSL
+            XslCompiledTransform xslt = new XslCompiledTransform();
+            xslt.Load(xslPath);
+            xslt.Transform(xmlPath, outputPath);
+
+            Console.WriteLine("Transformation complete. Output: " + outputPath);
         }
-
-        Console.WriteLine("XML validation complete.");
-
-        // 2. Transform XML to HTML using XSL
-        XslCompiledTransform xslt = new XslCompiledTransform();
-        xslt.Load(xslPath);
-        xslt.Transform(xmlPath, outputPath);
-
-        Console.WriteLine("Transformation complete. Output: " + outputPath);
+        catch (Exception ex)
+        {
+            Console.WriteLine("ERROR: " + ex.Message);
+        }
     }
 
     static void ValidationCallback(object sender, ValidationEventArgs e)
